7a0dc0458f00d01564818969ef1e5fe73e6d5b7e
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2023-12-17 02:18:24 +0300

    more changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,19 +6,25 @@ import java.net.InetAddress;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        // initialize the gateway
-        Gateway gateway = Gateway.getInstance(5000, 4445);
-
-        // initialize the sensors
-        TemperatureSensor tempSensor = new TemperatureSensor(20, 30);
-        HumiditySensor humiditySensor = new HumiditySensor(40, 90);
-
-        // returns 127.0.0.1
-        var localHostAddress = InetAddress.getLocalHost();
-
         // 2 different ports for UDP and TCP
         int udpPort = 4445;
         int tcpPort = 5000;
+
+        // initialize the gateway
+        Gateway gateway = Gateway.getInstance(tcpPort, udpPort);
+
+        // min-max temp/humidity values
+        int minTemp = 20;
+        int maxTemp = 30;
+        int minHumidity = 40;
+        int maxHumidity = 90;
+
+        // initialize the sensors
+        TemperatureSensor tempSensor = new TemperatureSensor(minTemp, maxTemp);
+        HumiditySensor humiditySensor = new HumiditySensor(minHumidity, maxHumidity);
+
+        // returns 127.0.0.1
+        var localHostAddress = InetAddress.getLocalHost();
 
         //  initialize the client instances with the corresponding sensors
         UDPClient humiditySensorClient = new UDPClient(humiditySensor, localHostAddress, udpPort);
